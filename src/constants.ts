61999c5c3111e40e1c563567263c249a28dcf0af
Sanchit Bhalla <sanchitbhalla91@gmail.com>
2025-06-22 03:00:07 +0530

    backend url updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-export const BACKEND_HOST = "http://localhost:3000";
+// export const BACKEND_HOST = "http://localhost:3000";
+export const BACKEND_HOST = "https://brainly.sanchitdev.com";
 
 export const APP_NAME = "iDE Brain"; // "Brainly"
 // export const APP_NAME = "Brainly";
